fc18aa141074b2ad812f5c9434e5932e5b09c0a9
osvaldsoza <osvald.soza@gmail.com>
2021-12-16 12:02:14 -0300

    implementacao rest template no hr-payroll

--- hr-payroll/src/main/java/br/com/monktec/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/br/com/monktec/hrpayroll/entities/Worker.java
@@ -1,23 +1,18 @@
-package br.com.monktec.hrworker.worker;
+package br.com.monktec.hrpayroll.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name = "tb_worker")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = {"name", "dailyIncome"})
 public class Worker implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double dailyIncome;
--- hr-payroll/src/main/java/br/com/monktec/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/monktec/hrpayroll/services/PaymentService.java
@@ -1,12 +1,34 @@
 package br.com.monktec.hrpayroll.services;
 
 import br.com.monktec.hrpayroll.entities.Payment;
+import br.com.monktec.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.00, days);
+
+        Map<String, String> urivariables = new HashMap<>();
+        urivariables.put("id", String.valueOf(workerId));
+
+        var worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, urivariables);
+
+        var name = worker.getName();
+        var dailyIncome = worker.getDailyIncome();
+
+        return new Payment(name, dailyIncome, days);
     }
 }
