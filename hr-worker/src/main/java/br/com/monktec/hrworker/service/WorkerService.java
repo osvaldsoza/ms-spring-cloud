310514694fd054790e35410a37a9665aea156468
osvaldsoza <osvald.soza@gmail.com>
2021-12-15 22:02:54 -0300

    refatoracao

--- hr-worker/src/main/java/br/com/monktec/hrworker/service/WorkerService.java
+++ hr-worker/src/main/java/br/com/monktec/hrworker/service/WorkerService.java
@@ -1,13 +1,28 @@
 package br.com.monktec.hrworker.service;
 
+import br.com.monktec.hrworker.repositories.WorkerRepository;
 import br.com.monktec.hrworker.worker.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
-public interface WorkerService {
+@Service
+public class WorkerService {
 
-    List<Worker> findAll();
+    @Autowired
+    private WorkerRepository workerRepository;
 
-    Worker findById(Long id);
+    public List<Worker> findAll() {
+        var workers = workerRepository.findAll();
+
+        return workers;
+    }
+
+    public Worker findById(Long id) {
+        var worker = workerRepository.findById(id).get();
+
+        return worker;
+    }
 }
