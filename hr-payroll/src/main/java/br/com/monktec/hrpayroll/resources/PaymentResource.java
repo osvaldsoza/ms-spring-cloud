b198ff708574a32ec0e46e229e8fd7181a9a9e2d
osvaldsoza <osvald.soza@gmail.com>
2022-03-30 17:31:04 -0300

    atualizacao

--- hr-payroll/src/main/java/br/com/monktec/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/monktec/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.com.monktec.hrpayroll.resources;
 
 import br.com.monktec.hrpayroll.entities.Payment;
 import br.com.monktec.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +20,19 @@ public class PaymentResource {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPayment2")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         var payment = paymentService.getPayment(workerId, days);
 
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPayment2(@PathVariable Long workerId, @PathVariable Integer days){
+        var payment = new Payment("Soza", 500.0,days);
+
+        return ResponseEntity.ok(payment);
+    }
+
+
 }
